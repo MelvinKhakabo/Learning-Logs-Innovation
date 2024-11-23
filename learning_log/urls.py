d0fe7ebb2565e1c123403a5c54ce13b5e226ca2f
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-11-23 19:32:29 +0300

    root folder

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -16,27 +16,20 @@ Including another URLconf
 """
 # learning_log/urls.py
 """URL configuration for learning_log project."""
-
-from django.contrib import admin
-from django.urls import path, include
-from . import views  # Make sure to import the views module for the home page view
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('learning_logs/', include('learning_logs.urls', namespace='learning_logs')),
-    path('users/', include('users.urls', namespace='users')),  # Include the users app URLs
-    path('', views.home_view, name='home'),  # Add this line for the home page URL
-]
-
 # learning_logs/urls.py
 from django.urls import path
 from users import views as user_views  # Import views from the 'users' app as 'user_views'
 
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
-    # Other paths...
-    path('users/login/', user_views.login_view, name='login'),  # Use user_views.login_view
-    # Other paths...
+    path('', include('learning_logs.urls', namespace='learning_logs')),  # Default to learning_logs home
+    path('users/', include('users.urls', namespace='users')),  # Include users app
+    path('admin/', admin.site.urls),
 ]
+
+
 
 
 
